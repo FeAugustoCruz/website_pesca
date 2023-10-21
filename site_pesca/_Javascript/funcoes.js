5c5dd7ca08fe655a677ebf212085b08877b87416
Julemarotaku3 <fernandin13augusto@gmail.com>
2023-10-21 18:58:26 -0300

    .

---
+++ site_pesca/_Javascript/funcoes.js
@@ -0,0 +1,8 @@
+function apareceLogo(){
+	document.getElementById("texto-contato").style.opacity = 1;
+	document.getElementById("texto-contato").style.borderColor = "white";
+}
+
+function desapareceLogo(){
+	document.getElementById("texto-contato").style.opacity = 0;
+}
